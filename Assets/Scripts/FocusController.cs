7c76c14a6eb89a395ea2b5ba46bd6b9df8e6db41
AMonoyios <43608041+AMonoyios@users.noreply.github.com>
2020-06-01 16:14:16 +0300

    20w23a
    Added on focusController, created the basic interaction point.
    Bug: Interaction point does not defocus

--- Assets/Scripts/FocusController.cs
+++ Assets/Scripts/FocusController.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class FocusController : MonoBehaviour
 {
     private Camera cam;
-
-    public LayerMask FocusMask;
     private float rayCastRange = 30.0f;
+
+    public InteractPoint focus;
+    public LayerMask FocusMask;
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +19,52 @@ public class FocusController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // when hitting the left mouse button 
         if (Input.GetMouseButtonDown(0))
         {
+            // creating a ray from the screen to a point in game
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
+            // if the ray hits a specific "layer" in game
             if (Physics.Raycast(ray, out hit, rayCastRange, FocusMask))
             {
-                Debug.Log("DEBUG: hit " + hit.collider.name);
-                // focus player    
+                InteractPoint interaction = hit.collider.GetComponent<InteractPoint>();
+
+                if (interaction != null)
+                {
+                    // focus player to specific item
+                    Debug.Log("DEBUG: focused " + hit.collider.name);
+                    
+                    Focus(interaction);
+                }
+                else
+                {
+                    // defocus player from any other focused items
+                    Debug.Log("DEBUG: defocused " + hit.collider.name);
+            
+                    DeFocus();
+                }
             }
+
+            //// if the ray hits a specific "layer" in game
+            //if (Physics.Raycast(ray, out hit, rayCastRange, FocusMask))
+            //{
+            //    InteractPoint interaction = hit.collider.GetComponent<InteractPoint>();
+            //    if (interaction != null)
+            //    {
+            //    }
+            //}
         }
+    }
+
+    void Focus (InteractPoint newFocus)
+    {
+        focus = newFocus;
+    }
+
+    void DeFocus()
+    {
+        focus = null;
     }
 }
---
+++ Assets/Scripts/InteractPoint.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class InteractPoint : MonoBehaviour
+{
+    [Range(1,4)]
+    public float radius = 2f;
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
