56c9a31f44bca9fb45c2261838a71f74c2008b63
AMonoyios <43608041+AMonoyios@users.noreply.github.com>
2020-06-01 15:15:21 +0300

    20w16a
    Created player, movement manager, camera manager, intro to raycast system

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;
+    public float pitch = 2.0f;
+
+    public float ZoomSpeed = 4.0f;
+    public float minZoom = 4.0f;
+    public float maxZoom = 13.0f;
+
+    private float currentZoom = 8.0f;
+
+    void Update()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * ZoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+    }
+
+    void LateUpdate()
+    {
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+    }
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float MovementSpeed;
+    public float RotationSpeed;
+
+    Quaternion newRotation;
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        
+        Vector3 movement = new Vector3(-moveVertical, 0.0f, moveHorizontal);
+
+        if (movement != Vector3.zero)
+            newRotation = Quaternion.LookRotation(movement);
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * RotationSpeed);
+            
+        transform.Translate(movement * MovementSpeed * Time.deltaTime, Space.World);
+    }
+}
